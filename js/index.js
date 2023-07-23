b51f89d144e1a8449a4e13fd8a1339f82f0bb7d2
hageramara <hageraliamara2002@gmail.com>
2023-07-24 02:01:16 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,75 @@
+var siteNameInput = document.getElementById("siteName");
+var siteUrlInput = document.getElementById("siteUrl");
+var dataContainer = [];
+
+if (localStorage.getItem('data') != null) {
+    dataContainer = JSON.parse(localStorage.getItem('data'));
+    displayData(dataContainer);
+}
+
+function addData() {
+    if (validation()) {
+        var data = {
+            name: siteNameInput.value,
+            url: siteUrlInput.value,
+        }
+        dataContainer.push(data);
+        localStorage.setItem('data', JSON.stringify(dataContainer))
+        console.log(dataContainer);
+        displayData(dataContainer);
+
+    }
+    clear();
+
+}
+
+function clear() {
+    siteNameInput.value = "";
+    siteUrlInput.value = "";
+}
+
+function displayData(arr) {
+    var box = ``;
+    for (var i = 0; i < arr.length; i++) {
+        box += `<tr>
+        <td>${i + 1}</td>
+     <td>${arr[i].name}</td>
+     <td><a href="#" target="_blank"><button class="btn btn-warning" onclick="visitSite(${i})";><i class="fa-solid fa-eye"></i>Visit</button></a></td>
+     <td><a href="#"><button class="btn btn-success" onclick="deleteData(${i})";> <i class="fa-solid fa-trash"></i> Delete</button></a></td>
+    </tr>`
+
+    }
+
+    document.getElementById("tableBody").innerHTML = box;
+}
+
+function deleteData(dataindex) {
+    dataContainer.splice(dataindex, 1);
+    localStorage.setItem("data", JSON.stringify(dataContainer))
+    displayData(dataContainer);
+    console.log(dataContainer);
+}
+
+function validation() {
+    var regex = /^((http|https):\/\/)?(www)?\.\w?[A-Za-z0-9]{2,9}.[a-z]{1,}$/
+    var letters = /^[A-Za-z]{2,}$/;
+
+    if (letters.test(siteNameInput.value) && regex.test(siteUrlInput.value)) {
+        siteNameInput.classList.add("is-valid")
+        siteUrlInput.classList.add("is-valid")
+        return true;
+    }
+
+    else {
+        alert("Site name must contain at least 3 characters");
+        alert("Site URL must be a valid one")
+        siteNameInput.classList.remove("is-valid")
+        siteUrlInput.classList.remove("is-valid")
+        return false;
+
+    }
+}
+
+function visitSite(i) {
+    window.open(dataContainer[i].url)
+}
